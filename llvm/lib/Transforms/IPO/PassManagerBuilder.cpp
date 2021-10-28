0818e891faca83d5f611184d0fd5e828ffc0c98f
Edd Barrett <vext01@gmail.com>
2021-10-28 11:29:57 +0100

    Convert control point pass to new pass manager.
    The pass previously used the legacy pass manager, since that was default
    in LLVM 12.
    
    In LLVM 13 the new pass manager is default. This change converts the
    control point pass from a legacy pass to a new pass.

--- llvm/lib/Transforms/IPO/PassManagerBuilder.cpp
+++ llvm/lib/Transforms/IPO/PassManagerBuilder.cpp
@@ -51,7 +51,6 @@
 #include "llvm/Transforms/Vectorize/LoopVectorize.h"
 #include "llvm/Transforms/Vectorize/SLPVectorizer.h"
 #include "llvm/Transforms/Vectorize/VectorCombine.h"
-#include "llvm/Transforms/Yk/ControlPoint.h"
 
 using namespace llvm;
 
@@ -188,11 +187,6 @@ cl::opt<AttributorRunOption> AttributorRun(
                           "enable call graph SCC attributor runs"),
                clEnumValN(AttributorRunOption::NONE, "none",
                           "disable attributor runs")));
-
-static cl::opt<bool>
-YkPatchCtrlPoint("yk-patch-control-point",
-  cl::init(false), cl::NotHidden,
-  cl::desc("Patch yk_control_point()"));
 
 extern cl::opt<bool> EnableKnowledgeRetention;
 } // namespace llvm
@@ -1245,13 +1239,6 @@ void PassManagerBuilder::populateLTOPassManager(legacy::PassManagerBase &PM) {
   addExtensionsToPM(EP_FullLinkTimeOptimizationLast, PM);
 
   PM.add(createAnnotationRemarksLegacyPass());
-
-  // We add the yk control point pass late in the pipeline (after all
-  // optimisation and just before verification and codegen) so that no IR
-  // optimisation passes have a chance to change the interface to the control
-  // point. The JIT runtime relies on the signature not being changed.
-  if (YkPatchCtrlPoint)
-    PM.add(createYkControlPointPass());
 
   if (VerifyOutput)
     PM.add(createVerifierPass());
