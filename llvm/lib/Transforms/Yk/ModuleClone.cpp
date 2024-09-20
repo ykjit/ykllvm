53b518951c3b38e4649b3de1825c460ced2679e5
Pavel Durov <pavel@p3ld3v.dev>
2024-09-20 15:22:53 +0100

    Remove globals update.
    Updating globals is no longer required since the use of the Linker:Flags
    flags. Llvm cloning mechanism will make sure to override the globabl
    from source.

--- llvm/lib/Transforms/Yk/ModuleClone.cpp
+++ llvm/lib/Transforms/Yk/ModuleClone.cpp
@@ -1,5 +1,4 @@
 #include "llvm/Transforms/Yk/ModuleClone.h"
-#include "llvm/Transforms/Yk/BasicBlockTracer.h"
 #include "llvm/IR/BasicBlock.h"
 #include "llvm/IR/Function.h"
 #include "llvm/IR/IRBuilder.h"
@@ -11,6 +10,7 @@
 #include "llvm/Linker/Linker.h"
 #include "llvm/Pass.h"
 #include "llvm/Transforms/Utils/Cloning.h"
+#include "llvm/Transforms/Yk/BasicBlockTracer.h"
 
 #define DEBUG_TYPE "yk-module-clone-pass"
 
@@ -29,43 +29,25 @@ struct YkModuleClone : public ModulePass {
   }
 
   void updateClonedFunctions(Module &M) {
-    std::vector<Function*> functionsToRemove;
+    std::vector<Function *> functionsToRemove;
     for (llvm::Function &F : M) {
       if (F.hasExternalLinkage() && F.isDeclaration()) {
         continue;
       }
       // Skip functions that are address taken
       if (!F.hasAddressTaken()) {
-        // Rename the function. This will lead to having multiple 
+        // Rename the function. This will lead to having multiple
         // verisons of the same function in the linked module.
         F.setName(YK_CLONE_PREFIX + F.getName().str());
       }
     }
   }
 
-  void updateClonedGlobals(Module &M) {
-    for (llvm::GlobalVariable &GV : M.globals()) {
-      if (GV.getName().str().find('.') == 0) {
-        continue; // This is likely not user-defined. Example: `.L.str`.
-      }
-      // Remove the initializer
-      GV.setInitializer(nullptr);
-      // Set external linkage
-      GV.setLinkage(llvm::GlobalValue::ExternalLinkage);
-    }
-  }
-
-
-
-  // This pass creates two distinct versions of the module functions. 
-  // It clones the module, updates functions and globals, and links the 
-  // cloned module to the original module.
-  // Functions:
-  // -  If function address is not taken, it is duplicated - resulting in 
-  //    two versions of that function in a module
-  // -  If functions address is taken, they remain as is.
-  // Globals:
-  // -  Global variables are not cloned.
+  // This pass creates two versions for module functions.
+  // original.
+  // If function address is not taken, it is duplicated - resulting in
+  // two versions of that function in a module. If function address is
+  // taken, it remain in its original form.
   bool runOnModule(Module &M) override {
     std::unique_ptr<Module> Cloned = CloneModule(M);
     if (!Cloned) {
@@ -74,13 +56,13 @@ struct YkModuleClone : public ModulePass {
       return false;
     }
     updateClonedFunctions(*Cloned);
-    updateClonedGlobals(*Cloned);
 
-    // The `OverrideFromSrc` flag tells the linker to 
+    // The `OverrideFromSrc` flag tells the linker to
     // prefer definitions from the source module (the 2nd argument) when
-    // there are conflicts. This means that if there are two functions 
+    // there are conflicts. This means that if there are two functions
     // with the same name, the one from the Cloned module will be used.
-    if (Linker::linkModules(M, std::move(Cloned), Linker::Flags::OverrideFromSrc)) {
+    if (Linker::linkModules(M, std::move(Cloned),
+                            Linker::Flags::OverrideFromSrc)) {
       llvm::report_fatal_error("Error linking the modules");
       return false;
     }
