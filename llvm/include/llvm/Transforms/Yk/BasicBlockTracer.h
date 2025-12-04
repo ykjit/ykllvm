fc2b633bf3eac883724bafb100c87ee6ee171949
Edd Barrett <vext01@gmail.com>
2025-12-04 11:18:36 +0000

    For the trace recorder, use a wrapper function with the PreserveAll ABI.
    We use a wrapper so we can use an ABI that disrupts register allocation
    in the interpreter code to a lesser extent. The wrapper is required
    because the actual recorder function is defined in the JIT runtime's
    shared object and we can't change the ABI for library calls.
    
    Benchmarking suggests some decent speedup for a handful of benchmarks,
    mostly in the 2%-10% range.

--- llvm/include/llvm/Transforms/Yk/BasicBlockTracer.h
+++ llvm/include/llvm/Transforms/Yk/BasicBlockTracer.h
@@ -5,6 +5,7 @@
 
 // The name of the trace function - used in swt tracing.
 #define YK_TRACE_FUNCTION "__yk_trace_basicblock"
+#define YK_TRACE_FUNCTION_WRAPPER "__yk_trace_basicblock_wrapper"
 
 // The name of the thread tracing state thread local.
 #define YK_THREAD_TRACING_STATE_TL "__yk_thread_tracing_state"
